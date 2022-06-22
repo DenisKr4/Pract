63bbabec9662f05a9a5b9b9c26ed0985dc91bc42
DenisKr4 <107630783+DenisKr4@users.noreply.github.com>
2022-06-22 15:54:36 +0300

    Add files via upload

---
+++ pract5/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+                Directory.CreateDirectory(@"C:\Folder\Folder_" + i.ToString());
+
+            Process.Start(@"C:\Folder");
+            Thread.Sleep(5000);
+
+            for (int i = 0; i < 100; i++)
+                Directory.Delete(@"C:\Folder\Folder_" + i.ToString());
+        }
+    }
+}
---
+++ pract5/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"C:\Users\user\Desktop\file.txt";
+            File.Create(path).Close();
+            File.WriteAllText(path, "Ghjbpdjmyst lfyyst\n\nПроизваольные данные!!1!");
+            Console.WriteLine(File.ReadAllText(path));
+            Console.ReadKey();
+        }
+    }
+}
